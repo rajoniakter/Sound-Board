9f9c81b84ec5324848269ad2921a2d82b2f1f376
rajoniakter <rajoniakter1709@gmail.com>
2023-08-04 01:57:35 +0600

    js

---
+++ script.js
@@ -0,0 +1,26 @@
+const sounds = ['data-reveal', 'evil-laugh', 'game-start', 'ping', 'whoah', 'wrong-answer']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').
+    appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
